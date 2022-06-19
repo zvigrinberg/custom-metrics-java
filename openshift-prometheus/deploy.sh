5a111354199d578b7941aee4eee11d41a600dbb7
zvigrinberg <zgrinberg@gmail.com>
2022-06-19 12:03:37 +0300

    grafanadashboard doesn't work with remote json file, turn it to embedded multi line string inline json inside grafanadashboard instance definition

--- openshift-prometheus/deploy.sh
+++ openshift-prometheus/deploy.sh
@@ -6,24 +6,27 @@ oc new-project custom-metric-test
 echo "Deploy pods on namespace..."
 echo
 oc apply -f pods.yaml
-echo "create services for the pods .."
+echo "Create services for the pods .."
 echo
 oc apply -f services.yaml
-echo "expose routes to services for the pods .."
+echo "Expose routes to services for the pods .."
 echo
 oc apply -f routes.yaml
-echo "enable user workloads on openshift monitoring:"
+echo "Enabling user workloads on openshift monitoring:"
 echo
 oc apply -f cluster-monitoring-config.yaml
 oc apply -f user-workload-monitoring-config.yaml
-echo "create Service Monitors to let Prometheus pull new application Metrics:"
+echo "Create Service Monitors to let Prometheus pull new application Metrics:"
 echo
 oc apply -f service-monitors.yaml
-echo "creating a datasource in grafana using the grafana operator and a grafanaDataSource CR instance:"
+echo "Creating a datasource in grafana using the grafana operator and a grafanaDataSource CR instance:"
 echo
 SECRET=$(oc get secret -n openshift-user-workload-monitoring | grep  prometheus-user-workload-token | head -n 1 | awk '{print $1 }')
 TOKEN=`echo $(oc get secret $SECRET -n openshift-user-workload-monitoring -o json | jq -r '.data.token') | base64 -d`
-sed 's/TOKEN_PLACEHOLDER/'""$TOKEN""'/g' grafana-data-source.yaml | oc apply -f - -n monitoring-poc
+sed 's/TOKEN_PLACEHOLDER/'""$TOKEN""'/g' grafana-data-source.yaml | oc apply -f - -n custom-metric-test
+
+echo "Creating a dashboard for application metrics"
+oc apply -f custom-java-metrics-dashboard.yaml
 echo "Done!"
 echo
 exit 0
