a2636547ba96edc71b81ac7ce4f38d45ec531fac
zvigrinberg <zgrinberg@gmail.com>
2022-06-19 18:36:25 +0300

    add new timer metric to java application

--- micrometer-java/src/main/java/com/redhat/examples/custommetrics/micrometer/demo/BookingService.java
+++ micrometer-java/src/main/java/com/redhat/examples/custommetrics/micrometer/demo/BookingService.java
@@ -1,13 +1,18 @@
 package com.redhat.examples.custommetrics.micrometer.demo;
 
+import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Timer;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 
 @Data
@@ -15,11 +20,13 @@ import java.util.List;
 public class BookingService {
 
     private final int factorConstant = 987298141;
+    private final int factorConstant2 = 827290631;
     private Counter numberOfBookings;
     private Counter amountOfMoney;
     private static BookingService bookingService=null;
     private Gauge successFailProportion;
     private List successFailProportionList;
+    private Timer transactionTime;
 
     public static BookingService getInstance(PrometheusMeterRegistry prometheusMeterRegistry) {
 
@@ -33,6 +40,11 @@ public class BookingService {
 
             bookingService.amountOfMoney = Counter.builder("amount_of_money")
                                                   .register(prometheusMeterRegistry);
+
+            bookingService.transactionTime = Timer.builder("transaction.time")
+                                             .description("times that takes to complete the transaction")
+                                             .register(prometheusMeterRegistry);
+
             bookingService.successFailProportionList = new ArrayList(){};
             bookingService.successFailProportionList.add(true);
             bookingService.successFailProportionList.add(false);
@@ -53,9 +65,11 @@ public class BookingService {
 
     public void makeBooking(String item,Integer money)
     {
+        Timer.Sample timer = Timer.start();
         numberOfBookings.increment();
         amountOfMoney.increment(money.doubleValue());
-        //randomize process of deciding whether the booking process was stable or not, with a chance of 1/5 that transaction will not be stable.
+        log.info("start time of tran:" + LocalDateTime.now());
+        //randomize process of deciding whether the booking process was stable or not, with a chance of 1/20 that transaction will not be stable.
         double randomNumber = Math.random() * factorConstant;
         int chance =  (int)randomNumber % 20;
         if (chance == 0)
@@ -66,9 +80,19 @@ public class BookingService {
         {
             successFailProportionList.add(true);
         }
+        double randomNumber2 = Math.random() * factorConstant2;
+        int sleepTime = (int)randomNumber2 % 548;
+        try {
+            log.info("processing the transaction time randomized value : = " + sleepTime);
+            sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-       log.info("Got request to make booking of item " + item + " with value of " + money );
 
+        log.info("Got request to make booking of item " + item + " with value of " + money );
+        log.info("end time of tran:" + LocalDateTime.now());
+       timer.stop(transactionTime);
     }
 
 }
